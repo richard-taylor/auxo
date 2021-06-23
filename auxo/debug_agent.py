eca75fe906520920a338ad9aa27e532974da94f3
Richard Taylor <rt@richardtaylor.co.uk>
2021-06-23 11:12:26 +0100

    Use a more readable date format for RunTime

--- auxo/debug_agent.py
+++ auxo/debug_agent.py
@@ -12,6 +12,6 @@ class RunTimeAgent(auxo.agent.BaseAgent):
         
     def result(self):
         report = super().result()
-        report.addText(datetime.datetime.now().isoformat()  + '\n')
+        report.addText(datetime.datetime.now().strftime('%H:%M %A %d-%m-%Y')  + '\n')
 
         return report
